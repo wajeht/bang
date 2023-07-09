2feea8f6d14fbd20481b9d9284095cc9dcb1cfe0
wajeht <58354193+wajeht@users.noreply.github.com>
2023-07-08 22:23:14 -0500

    feat: format on save

--- src/views/App.vue
+++ src/views/App.vue
@@ -3,5 +3,5 @@ import Test from './components/Test.vue';
 </script>
 
 <template>
-  <Test name="testing 1 2 trhee"/>
+	<Test name="testing 1 2 trhee" />
 </template>
--- src/views/components/Test.vue
+++ src/views/components/Test.vue
@@ -1,14 +1,12 @@
 <script setup lang="ts">
-
 import { reactive, ref } from 'vue';
 
 const name = ref('name');
 
 type Props = { name: string };
 const props = defineProps<Props>();
-
 </script>
 
 <template>
-  {{ props.name  }}
+	{{ props.name }}
 </template>
