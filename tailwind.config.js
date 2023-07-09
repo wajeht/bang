de897d4217adae01d7389c9f2b0d229f84d81f96
wajeht <58354193+wajeht@users.noreply.github.com>
2023-07-08 22:42:04 -0500

    feat: add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	important: true,
+  content: ['./src/views/index.html', './src/views/**/*.{vue,js,ts,jsx,tsx}'],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
