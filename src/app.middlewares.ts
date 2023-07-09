07a905996c61124aa2e2ef8f54dc1c8dc2c633c4
wajeht <58354193+wajeht@users.noreply.github.com>
2023-07-09 03:26:25 -0500

    feat: add build script

--- src/app.middlewares.ts
+++ src/app.middlewares.ts
@@ -1,4 +1,13 @@
+import path from 'path';
 import { Request, Response, NextFunction } from 'express';
+
+export async function vueHandler(req: Request, res: Response, next: NextFunction) {
+	try {
+		return res.sendFile(path.resolve(path.join(process.cwd(), 'public', 'index.html')));
+	} catch (e) {
+		next(e);
+	}
+}
 
 export function notFoundHandler(req: Request, res: Response, next: NextFunction) {
 	return res.status(404).send({
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,27 @@
+import path from 'path';
 import express from 'express';
+import cors from 'cors';
+import compression from 'compression';
+import helmet from 'helmet';
 const app = express();
 
 import api from './api/api.routes';
 import * as appMiddlewares from './app.middlewares';
 
+app.use(
+	helmet({
+		contentSecurityPolicy: false,
+		crossOriginEmbedderPolicy: false,
+	}),
+);
+app.use(cors());
+app.use(compression());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.resolve(path.join(process.cwd(), 'public')), { maxAge: '24h' }));
+
 app.use('/api', api);
+app.use('*', appMiddlewares.vueHandler);
 
 app.use(appMiddlewares.notFoundHandler);
 app.use(appMiddlewares.errorHandler);
