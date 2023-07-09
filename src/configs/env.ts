b631f7445fa8c402ef872cc08a313ff5fe7e4a52
wajeht <58354193+wajeht@users.noreply.github.com>
2023-07-09 03:56:27 -0500

    feat: prisma client

--- src/configs/env.ts
+++ src/configs/env.ts
@@ -5,19 +5,19 @@ import path from 'path';
 dotenv.config({ path: path.resolve(path.join(process.cwd(), '.env')) });
 
 const schema = z.object({
-  VUE_PORT: z.string().transform(Number),
-  SERVER_PORT: z.string().transform(Number),
-  NODE_ENV: z.enum(['production', 'development', 'testing']),
+	VUE_PORT: z.string().transform(Number),
+	SERVER_PORT: z.string().transform(Number),
+	NODE_ENV: z.enum(['production', 'development', 'testing']),
 });
 
 const parsed = schema.safeParse(process.env);
 
 if (!parsed.success) {
-  console.error(
-    '❌ Invalid environment variables:',
-    JSON.stringify(parsed.error.format(), null, 4),
-  );
-  process.exit(1);
+	console.error(
+		'❌ Invalid environment variables:',
+		JSON.stringify(parsed.error.format(), null, 4),
+	);
+	process.exit(1);
 }
 
 export default parsed.data;
--- src/database/index.ts
+++ src/database/index.ts
@@ -2,18 +2,18 @@ import { PrismaClient } from '@prisma/client';
 import ENV from '../configs/env';
 
 declare global {
-  // eslint-disable-next-line no-var
-  var prisma: PrismaClient | undefined;
+	// eslint-disable-next-line no-var
+	var prisma: PrismaClient | undefined;
 }
 
 const prisma =
-  global.prisma ||
-  new PrismaClient({
-    log: ENV.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
-  });
+	global.prisma ||
+	new PrismaClient({
+		log: ENV.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
+	});
 
 if (ENV.NODE_ENV !== 'production') {
-  global.prisma = prisma;
+	global.prisma = prisma;
 }
 
 export default prisma;
