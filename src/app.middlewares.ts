695436122e03b1bf201e58a68c366f7b7f01e887
wajeht <58354193+wajeht@users.noreply.github.com>
2023-07-08 19:55:42 -0500

    feat: app middle ware

--- src/app.middlewares.ts
+++ src/app.middlewares.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 
-export class AppMiddlewares {
+export default class AppMiddlewares {
 	public static notFoundHandler(req: Request, res: Response, next: NextFunction) {
 		return res.status(404).send({
 			message: 'Resource not found',
