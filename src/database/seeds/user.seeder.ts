ea609068f8800cdae5cf70c10a54a855e3d00f5a
wajeht <58354193+wajeht@users.noreply.github.com>
2023-07-09 05:22:16 -0500

    feat: fix prisma schema

--- src/database/seeds/user.seeder.ts
+++ src/database/seeds/user.seeder.ts
@@ -11,17 +11,17 @@ export async function userSeeder() {
 		// Generate new seeders
 		const usersData = Array.from({ length: 10 }, () => ({
 			username: faker.internet.userName(),
-			firstName: faker.person.firstName(),
-			lastName: faker.person.lastName(),
+			first_name: faker.person.firstName(),
+			last_name: faker.person.lastName(),
 			email: faker.internet.email(),
 			password: faker.internet.password(),
 			role: 'user',
 			verified: faker.datatype.boolean(),
-			verifiedAt: faker.date.recent(),
-			deletedAt: faker.date.recent(),
-			profilePictureUrl: faker.image.avatar(),
-			createdAt: faker.date.past(),
-			updatedAt: faker.date.recent(),
+			verified_at: faker.date.recent(),
+			deleted_at: faker.date.recent(),
+			profile_picture_url: faker.image.avatar(),
+			created_at: faker.date.past(),
+			updated_at: faker.date.recent(),
 		}));
 
 		await db.users.createMany({
