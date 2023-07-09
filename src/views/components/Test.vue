ebd1ccab2d33b38b23d3649672dc5d0b95184c9d
wajeht <58354193+wajeht@users.noreply.github.com>
2023-07-08 22:19:07 -0500

    feat: add vue

---
+++ src/views/components/Test.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+import { reactive, ref } from 'vue';
+
+const name = ref('name');
+
+type Props = { name: string };
+const props = defineProps<Props>();
+
+</script>
+
+<template>
+  {{ props.name  }}
+</template>
---
+++ vite.config.ts
@@ -0,0 +1,42 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: path.join(process.cwd(), '.env') });
+
+const rollupOptions = {};
+
+if (process.env.NODE_ENV === 'dev' || process.env.NODE_ENV === 'development') {
+	rollupOptions.output = {
+		entryFileNames: 'assets/[name].js',
+		chunkFileNames: 'assets/[name].js',
+		assetFileNames: 'assets/[name][extname]',
+	};
+}
+
+export default defineConfig({
+	plugins: [vue()],
+	root: './src/views/',
+	define: {
+		'process.env': process.env,
+	},
+	server: {
+		host: '0.0.0.0',
+		port: process.env.VUE_PORT as unknown as number,
+		proxy: {
+			'/api': {
+				target: `http://localhost:${process.env.SERVER_PORT}`,
+				changeOrigin: true,
+				rewrite: (path) => path.replace(/^\/api/, '/api'),
+			},
+		},
+	},
+	build: {
+		outDir: '../../public',
+		reportCompressedSize: true,
+		chunkSizeWarningLimit: 1600,
+		emptyOutDir: false,
+		rollupOptions,
+	},
+});
