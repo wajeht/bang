e40b4459301f2372bb7e0a7f9743578498ceb53b
wajeht <58354193+wajeht@users.noreply.github.com>
2023-07-08 20:27:00 -0500

    chore: language

--- src/server.ts
+++ src/server.ts
@@ -3,5 +3,5 @@ import app from './app';
 const PORT = 8080;
 
 app.listen(PORT, () => {
-	console.log(`Server is running on  http://localhost:${PORT}`);
+	console.log(`Server was started on http://localhost:${PORT}`);
 });
