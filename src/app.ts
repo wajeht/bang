bdbcd259192ab3c3e0b1d6d18ac597156ebd0ce8
wajeht <58354193+wajeht@users.noreply.github.com>
2023-07-08 20:28:11 -0500

    chore: language

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,11 @@ import express from 'express';
 const app = express();
 
 import api from './api/api.routes';
-import * as AppMiddlewares from './app.middlewares';
+import * as appMiddlewares from './app.middlewares';
 
 app.use('/api', api);
 
-app.use(AppMiddlewares.notFoundHandler);
-app.use(AppMiddlewares.errorHandler);
+app.use(appMiddlewares.notFoundHandler);
+app.use(appMiddlewares.errorHandler);
 
 export default app;
