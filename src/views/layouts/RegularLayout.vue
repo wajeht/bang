d9b1700059d7a833ae357d5d02fd7889981acd42
wajeht <58354193+wajeht@users.noreply.github.com>
2023-07-12 23:00:28 -0500

    feat: add fireworks

--- src/views/layouts/RegularLayout.vue
+++ src/views/layouts/RegularLayout.vue
@@ -1,7 +1,10 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+type Props = { class?: string };
+const props = withDefaults(defineProps<Props>(), { class: 'bg-base-200' });
+</script>
 
 <template>
-	<div class="bg-base-200 h-screen flex flex-col p-7 justify-between items-center">
+	<div :class="[props.class, 'h-screen flex flex-col p-7 justify-between items-center']">
 		<div></div>
 
 		<slot />
