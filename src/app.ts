dd23e4e3aa82cbea110d417e31b15a2c5dcf2445
wajeht <58354193+wajeht@users.noreply.github.com>
2023-07-14 00:44:11 -0500

    feat: api not found handler

--- src/app.ts
+++ src/app.ts
@@ -16,25 +16,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.resolve(path.join(process.cwd(), 'public')), { maxAge: '24h' }));
 
 app.use('/api', api);
-app.use(appMiddlewares.notFoundHandler);
 
-app.use((req, res, next) => {
-	// matching /api/v[number]/
-	const isApiPrefix = req.url.match(/\/api\/v\d\//g);
-
-	// console.log(req.url);
-	if (isApiPrefix) {
-		return res.status(StatusCodes.NOT_FOUND).send({
-			message: 'Resource not found',
-		});
-	}
-
-	next();
-});
+app.use(appMiddlewares.apiNotFoundHandle);
 
 app.use('*', appMiddlewares.vueHandler);
-app.use(appMiddlewares.notFoundHandler);
 
+app.use(appMiddlewares.notFoundHandler);
 app.use(appMiddlewares.errorHandler);
 
 export default app;
