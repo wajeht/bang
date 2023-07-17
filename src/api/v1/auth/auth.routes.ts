f6dd91dd66329199ee2ce7d8f4a9a4addfbcaf47
wajeht <58354193+wajeht@users.noreply.github.com>
2023-07-16 22:48:32 -0500

    feat: auth routes

--- src/api/v1/auth/auth.routes.ts
+++ src/api/v1/auth/auth.routes.ts
@@ -6,12 +6,24 @@ import { validate } from '../../api.middlewares';
 import * as authControllers from './auth.controllers';
 import * as authValidations from './auth.validations';
 
+auth.post('/login', validate({ body: authValidations.postLoginSchema }), authControllers.postLogin);
+
 auth.post(
 	'/register',
 	validate({ body: authValidations.postRegisterSchema }),
 	authControllers.postRegister,
 );
 
-auth.post('/login', validate({ body: authValidations.postLoginSchema }), authControllers.postLogin);
+auth.post(
+	'/reset-password',
+	validate({ body: authValidations.postResetPasswordSchema }),
+	authControllers.postLogin,
+);
+
+auth.post(
+	'/forgot-password',
+	validate({ body: authValidations.postForgotPasswordSchema }),
+	authControllers.postLogin,
+);
 
 export default auth;
