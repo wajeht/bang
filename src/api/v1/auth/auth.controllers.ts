3165f5dd72c0599057bf943134af0fd2431ddb8c
wajeht <58354193+wajeht@users.noreply.github.com>
2023-07-19 22:18:19 -0500

    chore: auth stuff

--- src/api/v1/auth/auth.controllers.ts
+++ src/api/v1/auth/auth.controllers.ts
@@ -1,7 +1,11 @@
 import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
+import * as AuthServices from './auth.services';
 
-export async function postRegister(req: Request, res: Response) {
+import type { PostRegisterSchema } from './auth.validations';
+
+export async function postRegister(req: Request<{}, {}, PostRegisterSchema>, res: Response) {
+	const user = await AuthServices.createUser(req.body);
 	return res.status(StatusCodes.OK).json({
 		message: 'postRegister() ok',
 	});
---
+++ src/api/v1/auth/auth.services.ts
@@ -0,0 +1,17 @@
+import { User, Role } from '../../../types/user';
+import db from '../../../database/db';
+
+export async function createUser(
+	user: Pick<User, 'email' | 'password' | 'username'>,
+): Promise<User> {
+	const createdUser = await db.user.create({
+		data: {
+			email: user.email,
+			password: user.password,
+			username: user.username,
+      role: Role.USER,
+		},
+	});
+
+	return createdUser;
+}
