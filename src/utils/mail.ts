51e7d6c9687cec1964cfbef05e0649562312c2b1
wajeht <58354193+wajeht@users.noreply.github.com>
2023-07-21 00:44:42 -0500

    feat: mailer

--- src/utils/mail.ts
+++ src/utils/mail.ts
@@ -1,7 +1,49 @@
 import nodemailer from 'nodemailer';
-
 import emailConfig from '../configs/mail.config';
 
-const mail = nodemailer.createTransport(emailConfig);
+interface MailData {
+	to: string;
+	subject: string;
+	html: string;
+}
 
-export default mail;
+interface WelcomeData {
+	name: string;
+}
+
+interface BirthdayData extends WelcomeData {
+	age: number;
+}
+
+const transporter = nodemailer.createTransport(emailConfig);
+
+function generateWelcomeHTML({ name }: WelcomeData): string {
+	return `
+    <h1>Welcome ${name}!</h1>
+    <p>Thanks for joining us.</p>
+  `;
+}
+
+function generateBirthdayHTML({ name, age }: BirthdayData): string {
+	return `
+    <h1>Happy Birthday ${name}!</h1>
+    <p>Congratulations on turning ${age}!</p>
+  `;
+}
+
+async function sendMail({ to, subject, html }: MailData): Promise<void> {
+	const mailOptions = {
+		from: process.env.EMAIL,
+		to,
+		subject,
+		html,
+	};
+
+	try {
+		await transporter.sendMail(mailOptions);
+	} catch (error) {
+		console.error('Error while sending mail: ', error);
+	}
+}
+
+export { sendMail, generateWelcomeHTML, generateBirthdayHTML };
