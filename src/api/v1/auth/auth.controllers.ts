58e191ed275d9e731d649f18926e6452468c4a9f
wajeht <58354193+wajeht@users.noreply.github.com>
2023-07-20 23:05:02 -0500

    feat: clear after loading

--- src/api/v1/auth/auth.controllers.ts
+++ src/api/v1/auth/auth.controllers.ts
@@ -4,27 +4,28 @@ import * as AuthServices from './auth.services';
 
 import type { PostRegisterSchema } from './auth.validations';
 
-export async function postRegister(req: Request<{}, {}, PostRegisterSchema>, res: Response) {
+export async function postRegister(req: Request<{}, {}, PostRegisterSchema>, res: Response): Promise<void> {
 	const user = await AuthServices.createUser(req.body);
-	return res.status(StatusCodes.OK).json({
+
+	 res.status(StatusCodes.OK).json({
 		message: 'postRegister() ok',
 	});
 }
 
-export async function postLogin(req: Request, res: Response) {
-	return res.status(StatusCodes.OK).json({
+export async function postLogin(req: Request, res: Response): Promise<void> {
+	 res.status(StatusCodes.OK).json({
 		message: 'postLogin() ok',
 	});
 }
 
-export async function postForgotPassword(req: Request, res: Response) {
-	return res.status(StatusCodes.OK).json({
+export async function postForgotPassword(req: Request, res: Response): Promise<void> {
+	 res.status(StatusCodes.OK).json({
 		message: 'postLogin() ok',
 	});
 }
 
-export async function postResetPassword(req: Request, res: Response) {
-	return res.status(StatusCodes.OK).json({
+export async function postResetPassword(req: Request, res: Response): Promise<void> {
+	 res.status(StatusCodes.OK).json({
 		message: 'postLogin() ok',
 	});
 }
--- src/api/v1/auth/auth.services.ts
+++ src/api/v1/auth/auth.services.ts
@@ -1,15 +1,23 @@
-import { User, Role } from '../../../types/user';
+import { User } from '../../../types/user';
 import db from '../../../database/db';
+import { ValidationError } from '../../../api/api.errors';
 
-export async function createUser(
-	user: Pick<User, 'email' | 'password' | 'username'>,
-): Promise<User> {
+export async function createUser(user: Pick<User, 'email' | 'password' | 'username'>) {
+	const foundUser = await db.user.findUnique({
+		where: {
+			email: user.email,
+		},
+	});
+
+	if (foundUser) {
+		throw new ValidationError('User already exists');
+	}
+
 	const createdUser = await db.user.create({
 		data: {
 			email: user.email,
 			password: user.password,
 			username: user.username,
-			role: Role.USER,
 		},
 	});
 
