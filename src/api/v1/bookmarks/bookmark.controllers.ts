6817fc47a395c04ad9e2e7d12612e213bab68fec
wajeht <58354193+wajeht@users.noreply.github.com>
2023-07-28 14:32:05 -0500

    fix: faker and scaffold bookmark api fetch

--- src/api/v1/bookmarks/bookmark.controllers.ts
+++ src/api/v1/bookmarks/bookmark.controllers.ts
@@ -1,6 +1,18 @@
 import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
+import { faker } from '@faker-js/faker';
 
 export async function getBookmarks(req: Request, res: Response): Promise<void> {
-    res.status(StatusCodes.OK).json({ message: 'ok' });
+	res.status(StatusCodes.OK).json({
+		message: 'ok',
+		data: Array.from({ length: 10 }, () => ({
+			id: faker.string.uuid(),
+			title: faker.lorem.sentence(),
+			url: faker.internet.url(),
+			description: faker.lorem.paragraph(),
+			image: faker.image.url(),
+			createdAt: faker.date.past(),
+			updatedAt: faker.date.recent(),
+		})),
+	});
 }
