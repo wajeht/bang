b615890e4ca1ab64214e3a305a9bf5861ee6b5c2
wajeht <58354193+wajeht@users.noreply.github.com>
2023-07-27 22:14:18 -0500

    feat: post forgot password test

--- src/api/v1/auth/auth.controllers.test.ts
+++ src/api/v1/auth/auth.controllers.test.ts
@@ -1,15 +1,17 @@
 import { StatusCodes } from 'http-status-codes';
 import { it, expect, describe, vi } from 'vitest';
-import { postRegister } from './auth.controllers';
-import { createUser } from './auth.services';
+import { postRegister, postForgotPassword } from './auth.controllers';
+import { createUser, setUserResetPasswordToken } from './auth.services';
 import * as mail from '../../../services/emails';
 
 vi.mock('./auth.services', async () => ({
 	createUser: vi.fn(),
+	setUserResetPasswordToken: vi.fn(),
 }));
 
 vi.mock('../../../services/emails', () => ({
 	sendVerifyEmail: vi.fn(),
+	sendResetPassword: vi.fn(),
 }));
 
 describe('postRegister', () => {
@@ -31,6 +33,26 @@ describe('postRegister', () => {
 			token: 'verification_token',
 			name: 'jaw',
 		});
+
+		expect(res.status).toHaveBeenCalledWith(StatusCodes.OK);
+		expect(res.json).toHaveBeenCalledWith({ message: 'ok' });
+	});
+});
+
+describe('postForgotPassword', () => {
+	it('should be able to send reset password email', async () => {
+		const req: any = { body: { email: 'jaw@email.com' } };
+		const res: any = { json: vi.fn(), status: vi.fn().mockReturnThis() };
+
+		await setUserResetPasswordToken.mockResolvedValue({
+			email: 'email@domail.com',
+			reset_password_token: 'reset_password_token',
+			username: 'jaw',
+		});
+
+		await postForgotPassword(req, res);
+
+		expect(setUserResetPasswordToken).toHaveBeenCalledWith(req.body.email);
 
 		expect(res.status).toHaveBeenCalledWith(StatusCodes.OK);
 		expect(res.json).toHaveBeenCalledWith({ message: 'ok' });
