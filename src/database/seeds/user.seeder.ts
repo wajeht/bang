c4c74aed1783226764f2f78c41cedccf50e70c62
wajeht <58354193+wajeht@users.noreply.github.com>
2023-07-29 06:23:35 -0500

    fix: update deps

--- src/database/seeds/user.seeder.ts
+++ src/database/seeds/user.seeder.ts
@@ -1,18 +1,20 @@
 import { faker } from '@faker-js/faker';
 import db from '../db';
+import { hashPassword } from '../../api/v1/auth/auth.utils';
 
 export async function userSeeder() {
 	try {
 		console.log('Dropping existing user records...');
 		// Delete existing records
 		await db.user.deleteMany();
+		const password = await hashPassword('password');
 
 		console.log('Creating new user seeders...');
 		// Generate new seeders
 		const usersData = Array.from({ length: 10 }, () => ({
 			username: faker.internet.userName(),
 			email: faker.internet.email(),
-			password: faker.internet.password(),
+			password,
 			verified: faker.datatype.boolean(),
 			verified_at: faker.date.recent(),
 			created_at: faker.date.past(),
