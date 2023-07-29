48d7b743f5ee0aa8967702bd53028f9e18a5806c
wajeht <58354193+wajeht@users.noreply.github.com>
2023-07-29 05:39:15 -0500

    fix: test

--- src/tests/vue-test-setup.ts
+++ src/tests/vue-test-setup.ts
@@ -6,16 +6,24 @@ export const setup = () => {
 
 process.env.VUE_PORT = '3000';
 process.env.SERVER_PORT = '8000';
-process.env.DB_URL = 'postgresql://username:password@localhost:5432/bang';
 process.env.NODE_ENV = 'testing';
-process.env.EMAIL_HOST = 'localhost';
-process.env.EMAIL_PORT = '1025';
-process.env.EMAIL_AUTH_EMAIL = 'example@example.com';
-process.env.EMAIL_AUTH_ALIAS = 'example@example.com';
-process.env.EMAIL_AUTH_PASS = 'your_email_password';
+
 process.env.PASSWORD_SALT = 'your_password_salt';
 process.env.DOMAIN = 'example.com';
 process.env.JWT_SECRET = 'your_jwt_secret';
 process.env.JWT_EXPIRES_IN = '1d';
 process.env.COOKIE_SECRET = 'your_cookie_secret';
 process.env.COOKIE_EXPIRES_IN = '30d';
+
+process.env.DB_PORT = '5432';
+process.env.DB_HOST = 'localhost';
+process.env.DB_USERNAME="username"
+process.env.DB_PASSWORD="password"
+process.env.DB_DATABASE="bang"
+process.env.DB_URL = 'postgresql://username:password@localhost:5432/bang';
+
+process.env.EMAIL_HOST = 'localhost';
+process.env.EMAIL_PORT = '1025';
+process.env.EMAIL_AUTH_EMAIL = 'example@example.com';
+process.env.EMAIL_AUTH_ALIAS = 'example@example.com';
+process.env.EMAIL_AUTH_PASS = 'your_email_password';
