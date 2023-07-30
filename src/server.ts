3ae8960ba80728a7ffdf7aba9f2dec56bdc4f068
wajeht <58354193+wajeht@users.noreply.github.com>
2023-07-30 02:18:40 -0500

    feat: disconnect db on kill

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
 import app from './app';
-
 import ENV from './configs/env';
+import db from './database/db'
 
 const server = app.listen(ENV.SERVER_PORT, async () => {
 	try {
-		// await db.init();
+		await db.$connect();
 		// await crons.init();
 		// await admin.init();
 
@@ -20,7 +20,7 @@ export async function gracefulShutdown() {
 	server.close(async () => {
 		try {
 			// redis.disconnect();
-			// await db.stop();
+			await db.$disconnect();
 			console.log('**** Closed out remaining connections. ****');
 			process.exit();
 		} catch (err) {
