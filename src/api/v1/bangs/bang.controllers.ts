d0a3aaa73db5b9a3d2f107248fcf557708f0fa15
wajeht <58354193+wajeht@users.noreply.github.com>
2023-07-30 02:09:25 -0500

    feat: type function return

--- src/api/v1/bangs/bang.controllers.ts
+++ src/api/v1/bangs/bang.controllers.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 
-export async function getQuery(req: Request, res: Response) {
+export async function getQuery(req: Request, res: Response): Promise<void> {
 	res.status(200).json({
 		message: 'ok',
 		data: [],
