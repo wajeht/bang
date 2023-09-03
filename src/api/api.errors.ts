10b7b14f03e11ba0d749f6552778081fedb02c0a
wajeht <58354193+wajeht@users.noreply.github.com>
2023-09-03 10:54:51 -0600

    feat: switch console to logger

--- src/api/api.errors.ts
+++ src/api/api.errors.ts
@@ -1,40 +1,40 @@
 import { StatusCodes } from 'http-status-codes';
 
 class HttpError {
-  public statusCode: StatusCodes;
+	public statusCode: StatusCodes;
 	public message: string;
-  constructor(statusCode: StatusCodes, message: string) {
-    this.statusCode = statusCode;
-    this.message = message;
-  }
+	constructor(statusCode: StatusCodes, message: string) {
+		this.statusCode = statusCode;
+		this.message = message;
+	}
 }
 
 export class ForbiddenError extends HttpError {
-  constructor(message: string) {
-    super(StatusCodes.FORBIDDEN, message);
-  }
+	constructor(message: string) {
+		super(StatusCodes.FORBIDDEN, message);
+	}
 }
 
 export class UnauthorizedError extends HttpError {
-  constructor(message: string) {
-    super(StatusCodes.UNAUTHORIZED, message);
-  }
+	constructor(message: string) {
+		super(StatusCodes.UNAUTHORIZED, message);
+	}
 }
 
 export class NotFoundError extends HttpError {
-  constructor(message: string) {
-    super(StatusCodes.NOT_FOUND, message);
-  }
+	constructor(message: string) {
+		super(StatusCodes.NOT_FOUND, message);
+	}
 }
 
 export class APICallsExceededError extends HttpError {
-  constructor(message: string) {
-    super(StatusCodes.TOO_MANY_REQUESTS, message);
-  }
+	constructor(message: string) {
+		super(StatusCodes.TOO_MANY_REQUESTS, message);
+	}
 }
 
 export class ValidationError extends HttpError {
-  constructor(message: string) {
-    super(StatusCodes.UNPROCESSABLE_ENTITY, message);
-  }
+	constructor(message: string) {
+		super(StatusCodes.UNPROCESSABLE_ENTITY, message);
+	}
 }
