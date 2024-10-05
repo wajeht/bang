6ffaa13eeb527dda00cf955b7a6debdf772f4a77
wajeht <58354193+wajeht@users.noreply.github.com>
2024-10-05 11:27:16 -0500

    refactor(main): Restructure main.go for improved code organization

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func getHomePageHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("hello"))
+}
 
 func main() {
 	fmt.Println("main.go")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", getHomePageHandler)
+	http.ListenAndServe(":8080", mux)
 }
