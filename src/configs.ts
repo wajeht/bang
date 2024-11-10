b4d23db4399173d2f4c51e5ddbd31845518e50e3
wajeht <58354193+wajeht@users.noreply.github.com>
2024-11-09 21:18:17 -0600

    refactor(handlers): Update handler functions to return responses consistently

--- src/configs.ts
+++ src/configs.ts
@@ -8,7 +8,6 @@ export const appConfig = {
 	port: parseInt(process.env.APP_PORT || '80', 10),
 	env: (process.env.APP_ENV as Env) || 'development',
 	appUrl: process.env.APP_URL || 'localhost',
-	apiKeySecret: process.env.APP_API_KEY_SECRET || 'bang',
 	secretSalt: process.env.APP_SECRET_SALT || 'bang',
 } as const;
 
--- src/handlers.ts
+++ src/handlers.ts
@@ -2,26 +2,31 @@ import { Request, Response } from 'express';
 
 // GET /
 export function getHomePageHandler(req: Request, res: Response) {
-	res.render('home.html', {
+	return res.render('home.html', {
 		path: '/',
 	});
 }
 
 // GET /healthz
 export function getHealthzHandler(req: Request, res: Response) {
+	if (req.get('Content-Type') === 'application/json') {
+		res.status(200).json({ message: 'ok' });
+		return;
+	}
+
 	res.setHeader('Content-Type', 'text/html').status(200).send('<p>ok</p>');
 }
 
 // GET /terms-of-service
 export function getTermsOfServicePageHandler(req: Request, res: Response) {
-	res.render('terms-of-service.html', {
+	return res.render('terms-of-service.html', {
 		path: '/terms-of-service',
 	});
 }
 
 // GET /privacy-policy
 export function getPrivacyPolicyPageHandler(req: Request, res: Response) {
-	res.render('privacy-policy', {
+	return res.render('privacy-policy', {
 		path: '/privacy-policy',
 	});
 }
