80506f6aae306c9d82393df88db7d329186c98e2
wajeht <58354193+wajeht@users.noreply.github.com>
2024-11-12 09:52:33 -0600

    feat: Add Dockerfile for production environment and update session config values

--- src/configs.ts
+++ src/configs.ts
@@ -35,7 +35,7 @@ export const oauthConfig = {
 } as const;
 
 export const sessionConfig = {
-	store_prefix: process.env.SESSION_STORE_PREFIX || 'notify',
-	secret: process.env.SESSION_SECRET || 'notify',
+	store_prefix: process.env.SESSION_STORE_PREFIX || 'bang',
+	secret: process.env.SESSION_SECRET || 'bang',
 	domain: process.env.SESSION_DOMAIN || 'localhost',
 } as const;
