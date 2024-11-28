bbb118dac57bc4ba161ab6ac78fdef66404cea51
wajeht <58354193+wajeht@users.noreply.github.com>
2024-11-27 22:57:06 -0600

    refactor: Improve toast creation and dismissal logic

--- public/script.js
+++ public/script.js
@@ -6,6 +6,7 @@ function updateButtonText(theme) {
 function toggleTheme() {
 	const currentTheme = document.documentElement.getAttribute('data-theme') || 'light';
 	const newTheme = currentTheme === 'light' ? 'dark' : 'light';
+
 	document.documentElement.setAttribute('data-theme', newTheme);
 	localStorage.setItem('theme', newTheme);
 	updateButtonText(newTheme);
@@ -17,90 +18,77 @@ function initializeTheme() {
 	updateButtonText(savedTheme);
 }
 
-function getData() {
-	const scriptTag = document.querySelector('script[data-state]');
-	const stateData = JSON.parse(scriptTag.getAttribute('data-state'));
-	return stateData;
-}
-
 function createToast(message) {
-	// Remove any existing toast
+	// Remove existing toast if present
 	const existingToast = document.getElementById('toast');
-	if (existingToast) {
-		existingToast.remove();
-	}
+	if (existingToast) existingToast.remove();
 
 	// Create new toast element
 	const toast = document.createElement('div');
 	toast.id = 'toast';
 	toast.style.cssText = `
-			position: fixed;
-			top: 20px;
-			right: -420px;
-			background-color: var(--background-color);
-			border: 2px solid var(--border-color);
-			color: var(--text-color);
-			padding: 20px;
-			border-radius: 5px;
-			transition: right 0.5s ease;
-			z-index: 20;
-			width: 100%;
-			min-width: 200px;
-			max-width: 400px;
-			text-align: center;
-			font-family: Arial, sans-serif;
-			cursor: pointer;
-			box-sizing: border-box;
+		position: fixed;
+		top: 20px;
+		right: -420px;
+		background-color: var(--background-color);
+		border: 2px solid var(--border-color);
+		color: var(--text-color);
+		padding: 20px;
+		border-radius: 5px;
+		transition: right 0.5s ease;
+		z-index: 20;
+		width: 100%;
+		min-width: 200px;
+		max-width: 400px;
+		text-align: center;
+		font-family: Arial, sans-serif;
+		cursor: pointer;
+		box-sizing: border-box;
 	`;
 	toast.textContent = decodeURIComponent(message);
 
-	// Add click event to dismiss toast
-	toast.onclick = function () {
-		dismissToast(toast);
-	};
+	// Add click event for manual dismissal
+	toast.onclick = () => dismissToast(toast);
 
-	// Add toast to body
+	// Add toast to body and animate
 	document.body.appendChild(toast);
+	setTimeout(() => (toast.style.right = '20px'), 100);
 
-	// Animate toast
-	setTimeout(() => {
-		toast.style.right = '20px';
-	}, 100);
-
-	// Set timeout to hide toast
-	setTimeout(() => {
-		dismissToast(toast);
-	}, 5000); // 5 sec
+	// Auto-dismiss toast after 5 seconds
+	setTimeout(() => dismissToast(toast), 5000);
 }
 
 function dismissToast(toast) {
 	toast.style.right = '-420px';
-	setTimeout(() => {
-		toast.remove();
-	}, 500);
+	setTimeout(() => toast.remove(), 500);
 }
 
 function initializeToast() {
+	// Display toast from URL parameters if exist
 	const urlParams = new URLSearchParams(window.location.search);
 	const toastMessage = urlParams.get('toast');
 	if (toastMessage) {
 		createToast(toastMessage);
 		urlParams.delete('toast');
-		const newUrl =
-			window.location.pathname + (urlParams.toString() ? '?' + urlParams.toString() : '');
+		const newUrl = `${window.location.pathname}${urlParams.toString() ? '?' + urlParams.toString() : ''}`;
 		history.replaceState({}, document.title, newUrl);
 	}
 
-	// flash message from session
-	const messages = getData();
-	for (const msg in messages) {
-		if (messages[msg].length) {
-			createToast(messages[msg]);
-		}
+	// Display flash messages from session if exist
+	const messages = getSessionData();
+	if (messages && Object.values(messages).some((msg) => msg.length > 0)) {
+		Object.values(messages).forEach((msg) => {
+			if (msg.length) createToast(msg);
+		});
 	}
 }
 
+function getSessionData() {
+	const scriptTag = document.querySelector('script[data-state]');
+	return JSON.parse(scriptTag.getAttribute('data-state'));
+}
+
 document.addEventListener('DOMContentLoaded', () => {
-	initializeToast();
 	initializeTheme();
+	initializeToast();
 });
