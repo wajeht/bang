b9feadd96b250a491b63f126f274f5e86ec16e2c
wajeht <58354193+wajeht@users.noreply.github.com>
2024-12-22 13:10:43 -0600

    feat(db): Add 'sessions' table creation migration with indexes

--- src/db/migrations/20241111003332_create_tables.ts
+++ src/db/migrations/20241111003332_create_tables.ts
@@ -1,6 +1,14 @@
 import type { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
+	await knex.schema.createTable('sessions', (table) => {
+		table.string('sid', 255).primary().notNullable();
+		table.json('sess').notNullable();
+		table.timestamp('expired').notNullable();
+
+		table.index(['expired'], 'sessions_expired_index');
+	});
+
 	await knex.schema.createTable('users', (table) => {
 		table.increments('id').primary();
 		table.string('username').unique().notNullable();
@@ -42,14 +50,6 @@ export async function up(knex: Knex): Promise<void> {
 		table.index(['user_id', 'trigger']);
 	});
 
-	await knex.schema.createTable('sessions', (table) => {
-		table.string('sid', 255).primary().notNullable();
-		table.json('sess').notNullable();
-		table.timestamp('expired').notNullable();
-
-		table.index(['expired'], 'sessions_expired_index');
-	});
-
 	await knex('action_types').insert([
 		{ name: 'search' },
 		{ name: 'redirect' },
@@ -58,9 +58,9 @@ export async function up(knex: Knex): Promise<void> {
 }
 
 export async function down(knex: Knex): Promise<void> {
-	await knex.schema.dropTableIfExists('sessions');
 	await knex.schema.dropTableIfExists('bangs');
 	await knex.schema.dropTableIfExists('bookmarks');
 	await knex.schema.dropTableIfExists('action_types');
 	await knex.schema.dropTableIfExists('users');
+	await knex.schema.dropTableIfExists('sessions');
 }
