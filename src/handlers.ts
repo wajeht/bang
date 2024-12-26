14be363d52709036e8eceb50482f6e47016a0f41
wajeht <58354193+wajeht@users.noreply.github.com>
2024-12-25 18:08:06 -0600

    style: Update styling for video player in home.html

--- src/handlers.ts
+++ src/handlers.ts
@@ -199,10 +199,9 @@ export async function postSearchHandler(req: Request, res: Response) {
 // GET /
 export async function getHomePageAndSearchHandler(req: Request, res: Response) {
 	const query = req.query.q?.toString().trim() || '';
-	let user;
+	const user = req.session.user;
 
 	if (!query) {
-		user = await db.select('*').from('users').where('id', req.session.user!.id).first();
 		if (!user) {
 			return res.render('home.html', {
 				path: '/',
