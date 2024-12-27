f8686a53c4df0edc79215e2e1812808e7cbec341
wajeht <58354193+wajeht@users.noreply.github.com>
2024-12-27 13:25:16 -0600

    feat(playwright): Add Playwright test configuration and browser tests for homepage

---
+++ src/tests/browser/home-pagte.test.ts
@@ -0,0 +1,22 @@
+import { test, expect } from '@playwright/test';
+
+test('can get / page', async ({ page }) => {
+	await page.goto('/');
+	await expect(page).toHaveTitle(/Bang - DuckDuckGo's !Bangs, but on steroids./);
+});
+
+test('can login and redirect to GitHub', async ({ page }) => {
+	await page.goto('/');
+
+	await page.getByRole('button', { name: '🚀 Get Started' }).click();
+
+	await expect(page.getByText('🔐 Join the Bang Gang!')).toBeVisible();
+
+	const githubPromise = page.waitForURL('https://github.com/**');
+
+	await page.getByRole('button', { name: '🐙 Bang with GitHub' }).click();
+
+	await githubPromise;
+
+	expect(page.url()).toContain('github.com');
+});
