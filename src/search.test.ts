0f4595704b2d9781a5827c3a8a30b5b225543f37
wajeht <58354193+wajeht@users.noreply.github.com>
2025-02-17 22:50:51 -0600

    feat(test): Add tests for search function exceeding search limits and slowing down

--- src/search.test.ts
+++ src/search.test.ts
@@ -98,5 +98,67 @@ describe('search', () => {
 			expect(req.session.cumulativeDelay).toBe(5000);
 			expect(req.session.user).toBeUndefined();
 		});
+
+		it('should redirect back with a warning when a user has reached to its 60th search', async () => {
+			const req = {
+				session: {
+					searchCount: 60,
+					cumulativeDelay: 5000,
+				},
+			} as unknown as Request;
+
+			const res = {
+				status: vi.fn().mockReturnThis(),
+				redirect: vi.fn(),
+				setHeader: vi.fn().mockReturnThis(),
+				send: vi.fn(),
+			} as unknown as Response;
+
+			await search({ req, res, user: undefined, query: '!g python' });
+
+			expect(res.status).toHaveBeenCalledWith(200);
+			expect(res.setHeader).toHaveBeenCalledWith('Content-Type', 'text/html');
+			expect(res.send).toHaveBeenCalledWith(
+				expect.stringContaining(
+					"You've exceeded the search limit for unauthenticated users. Please log in for unlimited searches without delays.",
+				),
+			);
+			expect(res.send).toHaveBeenCalledWith(
+				expect.stringContaining('window.location.href = "https://duckduckgo.com/?q=python"'),
+			);
+			expect(req.session.searchCount).toBe(61);
+			expect(req.session.cumulativeDelay).toBe(10000);
+			expect(req.session.user).toBeUndefined();
+		});
+
+		it('should have slow down the search when a user has reached more than 60 searches', async () => {
+			const req = {
+				session: {
+					searchCount: 61,
+					cumulativeDelay: 5000,
+				},
+			} as unknown as Request;
+
+			const res = {
+				status: vi.fn().mockReturnThis(),
+				redirect: vi.fn(),
+				setHeader: vi.fn().mockReturnThis(),
+				send: vi.fn(),
+			} as unknown as Response;
+
+			await search({ req, res, user: undefined, query: '!g python' });
+
+			expect(res.status).toHaveBeenCalledWith(200);
+			expect(res.setHeader).toHaveBeenCalledWith('Content-Type', 'text/html');
+			expect(res.send).toHaveBeenCalledWith(
+				expect.stringContaining('Your next search will be slowed down for 10 seconds.'),
+			);
+			expect(res.send).toHaveBeenCalledWith(
+				expect.stringContaining('window.location.href = "https://www.google.com/search?q=python"'),
+			);
+			expect(req.session.searchCount).toBe(62);
+			expect(req.session.cumulativeDelay).toBe(10000);
+			expect(req.session.user).toBeUndefined();
+		});
 	});
 });
