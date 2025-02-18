55dd330a02759e4a5ea6a07aaea94996973cf2cf
wajeht <58354193+wajeht@users.noreply.github.com>
2025-02-17 22:34:38 -0600

    test(search): Add tests for redirecting to Google based on search query

--- src/search.test.ts
+++ src/search.test.ts
@@ -3,23 +3,44 @@ import { afterAll, beforeAll, describe, expect, it, vi } from 'vitest';
 import { search } from './search';
 
 describe('search', () => {
-	it('should work', async () => {
-		const req = {
-			session: {
-				searchCount: 0,
-			},
-			params: {
-				q: '!g python',
-			},
-		} as unknown as Request;
+	describe('unauthenticated user flow', () => {
+		it('should redirect to google when !g is used', async () => {
+			const req = {
+				session: {
+					searchCount: 0,
+				},
+				params: {
+					q: '!g python',
+				},
+			} as unknown as Request;
 
-		const res = {
-			status: 200,
-			redirect: vi.fn(),
-		} as unknown as Response;
+			const res = {
+				status: 200,
+				redirect: vi.fn(),
+			} as unknown as Response;
 
-		await search({ req, res, user: undefined, query: '!g python' });
+			await search({ req, res, user: undefined, query: '!g python' });
 
-		expect(res.status).toBe(200);
+			expect(res.status).toBe(200);
+			expect(res.redirect).toHaveBeenCalledWith('https://www.google.com/search?q=python');
+		});
+
+		it('should redirect to google when !g is used without a search term', async () => {
+			const req = {
+				session: {
+					searchCount: 0,
+				},
+			} as unknown as Request;
+
+			const res = {
+				status: 200,
+				redirect: vi.fn(),
+			} as unknown as Response;
+
+			await search({ req, res, user: undefined, query: '!g' });
+
+			expect(res.status).toBe(200);
+			expect(res.redirect).toHaveBeenCalledWith('https://www.google.com');
+		});
 	});
 });
