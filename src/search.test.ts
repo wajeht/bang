d503ce03f6b26860a136d5a61229d6b3f90a87f7
wajeht <58354193+wajeht@users.noreply.github.com>
2025-02-17 22:44:34 -0600

    test(search): Add test cases for new search functionality and edge cases

--- src/search.test.ts
+++ src/search.test.ts
@@ -23,6 +23,8 @@ describe('search', () => {
 
 			expect(res.status).toBe(200);
 			expect(res.redirect).toHaveBeenCalledWith('https://www.google.com/search?q=python');
+			expect(req.session.searchCount).toBe(1);
+			expect(req.session.user).toBeUndefined();
 		});
 
 		it('should redirect to google when !g is used without a search term', async () => {
@@ -41,9 +43,11 @@ describe('search', () => {
 
 			expect(res.status).toBe(200);
 			expect(res.redirect).toHaveBeenCalledWith('https://www.google.com');
+			expect(req.session.searchCount).toBe(1);
+			expect(req.session.user).toBeUndefined();
 		});
 
-		it('should redirect ddg without a exclaimation mark when !doesnotecistanywhere is used', async () => {
+		it('should redirect ddg without a exclamation mark when !doesnotexistanywhere is used', async () => {
 			const req = {
 				session: {
 					searchCount: 0,
@@ -55,10 +59,44 @@ describe('search', () => {
 				redirect: vi.fn(),
 			} as unknown as Response;
 
-			await search({ req, res, user: undefined, query: '!doesnotecistanywhere' });
+			await search({ req, res, user: undefined, query: '!doesnotexistanywhere' });
 
 			expect(res.status).toBe(200);
-			expect(res.redirect).toHaveBeenCalledWith('https://duckduckgo.com/?q=!doesnotecistanywhere');
+			expect(res.redirect).toHaveBeenCalledWith('https://duckduckgo.com/?q=!doesnotexistanywhere');
+			expect(req.session.searchCount).toBe(1);
+			expect(req.session.user).toBeUndefined();
+		});
+
+		it('should redirect back with a warning when a user has reached to its 10th search', async () => {
+			const req = {
+				session: {
+					searchCount: 10,
+					cumulativeDelay: 5000,
+				},
+			} as unknown as Request;
+
+			const res = {
+				status: vi.fn().mockReturnThis(),
+				redirect: vi.fn(),
+				setHeader: vi.fn().mockReturnThis(),
+				send: vi.fn(),
+			} as unknown as Response;
+
+			await search({ req, res, user: undefined, query: '!g python' });
+
+			expect(res.status).toHaveBeenCalledWith(200);
+			expect(res.setHeader).toHaveBeenCalledWith('Content-Type', 'text/html');
+			expect(res.send).toHaveBeenCalledWith(
+				expect.stringContaining(
+					'You have used 10 out of 60 searches. Log in for unlimited searches!',
+				),
+			);
+			expect(res.send).toHaveBeenCalledWith(
+				expect.stringContaining('window.location.href = "https://duckduckgo.com/?q=python"'),
+			);
+			expect(req.session.searchCount).toBe(11);
+			expect(req.session.cumulativeDelay).toBe(5000);
+			expect(req.session.user).toBeUndefined();
 		});
 	});
 });
