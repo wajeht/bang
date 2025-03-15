481a3ac2442f8b12a4a2f49eefa0d93bbb2e26dd
wajeht <wajeht@users.noreply.github.com>
2025-03-15 18:29:06 +0000

    style: format codes

--- public/script.js
+++ public/script.js
@@ -101,7 +101,7 @@ function initializeToast() {
         for (const msg of Object.values(flash)) {
             if (msg?.length) createToast(msg);
         }
-    } catch { }
+    } catch {}
 }
 
 /**
@@ -165,13 +165,13 @@ function initializeDetailsElements() {
 
     if (detailsElements.length > 0) {
         // For each details element
-        detailsElements.forEach(detailsElement => {
+        detailsElements.forEach((detailsElement) => {
             // Event listener for opening this details element
-            detailsElement.addEventListener('toggle', function(event) {
+            detailsElement.addEventListener('toggle', function (event) {
                 // If this details element was just opened
                 if (this.hasAttribute('open')) {
                     // Close all other details elements
-                    detailsElements.forEach(otherDetails => {
+                    detailsElements.forEach((otherDetails) => {
                         if (otherDetails !== this && otherDetails.hasAttribute('open')) {
                             otherDetails.removeAttribute('open');
                         }
@@ -181,11 +181,11 @@ function initializeDetailsElements() {
         });
 
         // Document-level click handler to close all details when clicking outside
-        document.addEventListener('click', function(event) {
+        document.addEventListener('click', function (event) {
             // Check if the click was inside any details element
             let clickedInsideAnyDetails = false;
 
-            detailsElements.forEach(details => {
+            detailsElements.forEach((details) => {
                 if (event.target instanceof Node && details.contains(event.target)) {
                     clickedInsideAnyDetails = true;
                 }
@@ -193,7 +193,7 @@ function initializeDetailsElements() {
 
             // If clicked outside all details elements, close any open ones
             if (!clickedInsideAnyDetails) {
-                detailsElements.forEach(details => {
+                detailsElements.forEach((details) => {
                     if (details.hasAttribute('open')) {
                         details.removeAttribute('open');
                     }
