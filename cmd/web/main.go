d26944f6d4e83a1a8bbced3a68bb954270c21215
wajeht <58354193+wajeht@users.noreply.github.com>
2025-04-09 12:02:55 -0500

    feat(main): Add application struct and Run function to main.go

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,8 +2,23 @@ package main
 
 import (
 	"fmt"
+	"log/slog"
 )
 
-func main() {
-	fmt.Println("web.go")
+type config struct {
+	baseUrl string
+	httpPort int
+}
+
+type application struct {
+	config config
+	logger *slog.Logger
+}
+
+func Run(logger *slog.Logger) error {
+
+	var cfg config
+
+	cfg.baseUrl = 
+
 }
