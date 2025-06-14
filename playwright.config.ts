603671fd11f312fbaba2c17c69c6fb8af844c2e5
wajeht <58354193+wajeht@users.noreply.github.com>
2025-06-13 21:38:17 -0500

    refactor(playwright): update variable names for clarity and consistency

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,4 @@
-import { appConfig } from './src/config';
+import { config } from './src/config';
 import { defineConfig, devices } from '@playwright/test';
 
 /**
@@ -26,7 +26,7 @@ export default defineConfig({
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
         /* Base URL to use in actions like `await page.goto('/')`. */
-        baseURL: `http://127.0.0.1:${appConfig.port}`,
+        baseURL: `http://127.0.0.1:${config.app.port}`,
 
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
         trace: 'on-first-retry',
@@ -73,7 +73,7 @@ export default defineConfig({
     /* Run your local dev server before starting the tests */
     webServer: {
         command: 'npm run dev',
-        url: `http://127.0.0.1:${appConfig.port}`,
+        url: `http://127.0.0.1:${config.app.port}`,
         reuseExistingServer: !process.env.CI,
     },
 });
