4990075795e40db4aa26e7518e1c7aeaa84a2c14
wajeht <58354193+wajeht@users.noreply.github.com>
2025-06-17 20:12:42 -0500

    feat(server): implement server creation and shutdown functionality

--- src/app.ts
+++ src/app.ts
@@ -13,48 +13,145 @@ import express from 'express';
 import { router } from './router';
 import flash from 'connect-flash';
 import { config } from './config';
+import { logger } from './logger';
+import { Server } from 'node:http';
+import { checkMailpit } from './util';
 import compression from 'compression';
+import { AddressInfo } from 'node:net';
+import { db, runMigrations } from './db/db';
 import expressLayouts from 'express-ejs-layouts';
 import { expressJSDocSwaggerHandler, swagger } from './util';
 import { expressTemplatesReload as reload } from '@wajeht/express-templates-reload';
 
-const app = express()
-    .set('trust proxy', 1)
-    .use(sessionMiddleware())
-    .use(flash())
-    .use(compression())
-    .use(cors())
-    .use(helmetMiddleware())
-    .use(rateLimitMiddleware())
-    .use(express.json({ limit: '10mb' }))
-    .use(express.urlencoded({ extended: true, limit: '10mb' }))
-    .use(express.static('./public', { maxAge: '30d', etag: true, lastModified: true }))
-    .engine('html', ejs.renderFile)
-    .set('view engine', 'html')
-    .set('view cache', config.app.env === 'production')
-    .set('views', './src/views/pages')
-    .set('layout', '../layouts/public.html')
-    .use(expressLayouts)
-    .use(...csrfMiddleware)
-    .use(appLocalStateMiddleware);
+export async function createServer() {
+    const app = express()
+        .set('trust proxy', 1)
+        .use(sessionMiddleware())
+        .use(flash())
+        .use(compression())
+        .use(cors())
+        .use(helmetMiddleware())
+        .use(rateLimitMiddleware())
+        .use(express.json({ limit: '10mb' }))
+        .use(express.urlencoded({ extended: true, limit: '10mb' }))
+        .use(express.static('./public', { maxAge: '30d', etag: true, lastModified: true }))
+        .engine('html', ejs.renderFile)
+        .set('view engine', 'html')
+        .set('view cache', config.app.env === 'production')
+        .set('views', './src/views/pages')
+        .set('layout', '../layouts/public.html')
+        .use(expressLayouts)
+        .use(...csrfMiddleware)
+        .use(appLocalStateMiddleware);
 
-if (config.app.env === 'development') {
-    reload({
-        app,
-        options: { quiet: false },
-        watch: [
-            { path: './public/style.css' },
-            { path: './public/script.js' },
-            { path: './src/views', extensions: ['.html'] },
-        ],
+    if (config.app.env === 'development') {
+        reload({
+            app,
+            options: { quiet: false },
+            watch: [
+                { path: './public/style.css' },
+                { path: './public/script.js' },
+                { path: './src/views', extensions: ['.html'] },
+            ],
+        });
+    }
+
+    app.use(router);
+
+    expressJSDocSwaggerHandler(app, swagger);
+
+    app.use(notFoundMiddleware());
+    app.use(errorMiddleware());
+
+    const server: Server = app.listen(config.app.port);
+
+    server.timeout = 120000; // 2 minutes
+    server.keepAliveTimeout = 65000; // 65 seconds
+    server.headersTimeout = 66000; // slightly higher than keepAliveTimeout
+    server.requestTimeout = 120000; // same as timeout
+
+    server.on('listening', async () => {
+        const addr: string | AddressInfo | null = server.address();
+        const bind: string =
+            typeof addr === 'string' ? 'pipe ' + addr : 'port ' + (addr as AddressInfo).port;
+
+        logger.info(`Server is listening on ${bind}`);
+        if (config.app.env === 'development') {
+            const isMailpitRunning = await checkMailpit();
+            if (isMailpitRunning) {
+                logger.info('mailpit is running on http://localhost:8025');
+            }
+        }
+
+        if (config.app.env === 'production') {
+            try {
+                await db.raw('SELECT 1');
+                logger.info('Database connection verified before migrations');
+                await runMigrations();
+                logger.info('Database migrations completed successfully');
+            } catch (error) {
+                logger.error('Database connection or migration error: %o', error);
+            }
+        }
     });
+
+    server.on('error', (error: NodeJS.ErrnoException) => {
+        if (error.syscall !== 'listen') {
+            throw error;
+        }
+
+        const bind: string =
+            typeof config.app.port === 'string'
+                ? 'Pipe ' + config.app.port
+                : 'Port ' + config.app.port;
+
+        switch (error.code) {
+            case 'EACCES':
+                logger.error(`${bind} requires elevated privileges`);
+                process.exit(1);
+                break;
+            case 'EADDRINUSE':
+                logger.error(`${bind} is already in use`);
+                process.exit(1);
+                break;
+            default:
+                throw error;
+        }
+    });
+
+    return { app, server };
 }
 
-app.use(router);
+export async function closeServer({ server }: { server: Server }) {
+    logger.info('Shutting down server gracefully...');
 
-expressJSDocSwaggerHandler(app, swagger);
+    let shutdownComplete = false;
 
-app.use(notFoundMiddleware());
-app.use(errorMiddleware());
+    server.keepAliveTimeout = 0;
+    server.headersTimeout = 0;
+    server.timeout = 1;
 
-export { app };
+    server.close(async () => {
+        logger.info('HTTP server closed.');
+
+        try {
+            await db.destroy();
+            logger.info('[closeServer]: Database connection closed.');
+
+            shutdownComplete = true;
+            logger.info('[closeServer]: All connections closed successfully.');
+        } catch (error) {
+            logger.error(`[closeServer]: Error during shutdown: %o`, error);
+            throw error;
+        }
+    });
+
+    setTimeout(() => {
+        if (!shutdownComplete) {
+            logger.error(
+                '[closeServer]: Could not close connections in time, forcefully shutting down',
+            );
+            process.exit(1);
+        }
+    }, 10000);
+}
--- src/server.ts
+++ src/server.ts
@@ -1,133 +1,43 @@
-import { app } from './app';
+import { createServer, closeServer } from './app';
 import { logger } from './logger';
 import { Request } from 'express';
-import { Server } from 'node:http';
 import { config } from './config';
-import { AddressInfo } from 'node:net';
-import { db, runMigrations } from './db/db';
-import { sendNotification, checkMailpit } from './util';
+import { sendNotification } from './util';
 
-const server: Server = app.listen(config.app.port);
+export async function run() {
+    process.title = 'bang';
 
-process.title = 'bang';
+    const serverInfo = await createServer();
 
-server.timeout = 120000; // 2 minutes
-server.keepAliveTimeout = 65000; // 65 seconds
-server.headersTimeout = 66000; // slightly higher than keepAliveTimeout
-server.requestTimeout = 120000; // same as timeout
+    const gracefulShutdown = async (signal: string) => {
+        logger.info(`Received ${signal}, shutting down gracefully.`);
 
-server.on('listening', async () => {
-    const addr: string | AddressInfo | null = server.address();
-    const bind: string = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + (addr as AddressInfo).port; // prettier-ignore
-
-    logger.info(`Server is listening on ${bind}`);
-    if (config.app.env === 'development') {
-        const isMailpitRunning = await checkMailpit();
-        if (isMailpitRunning) {
-            logger.info('mailpit is running on http://localhost:8025');
-        }
-    }
-
-    if (config.app.env === 'production') {
-        try {
-            await db.raw('SELECT 1');
-            logger.info('Database connection verified before migrations');
-            await runMigrations();
-            logger.info('Database migrations completed successfully');
-        } catch (error) {
-            logger.error('Database connection or migration error: %o', error);
-        }
-    }
-});
-
-server.on('error', (error: NodeJS.ErrnoException) => {
-    if (error.syscall !== 'listen') {
-        throw error;
-    }
-
-    const bind: string = typeof config.app.port === 'string' ? 'Pipe ' + config.app.port : 'Port ' + config.app.port; // prettier-ignore
-
-    switch (error.code) {
-        case 'EACCES':
-            logger.error(`${bind} requires elevated privileges`);
+        setTimeout(() => {
+            logger.error(
+                '[gracefulShutdown]: Could not close connections in time, forcefully shutting down',
+            );
             process.exit(1);
-        // eslint-disable-next-line no-fallthrough
-        case 'EADDRINUSE':
-            logger.error(`${bind} is already in use`);
-            process.exit(1);
-        // eslint-disable-next-line no-fallthrough
-        default:
-            throw error;
-    }
-});
-
-function gracefulShutdown(signal: string): void {
-    logger.info(`Received ${signal}, shutting down gracefully.`);
-
-    let shutdownComplete = false;
-
-    server.keepAliveTimeout = 0;
-    server.headersTimeout = 0;
-    server.timeout = 1;
-
-    server.close(async () => {
-        logger.info('HTTP server closed.');
+        }, 10000).unref();
 
         try {
-            await db.destroy();
-            logger.info('[gracefulShutdown]: Database connection closed.');
-
-            shutdownComplete = true;
-            logger.info('[gracefulShutdown]: All connections closed successfully.');
+            await closeServer(serverInfo);
             process.exit(0);
         } catch (error) {
             logger.error(`[gracefulShutdown]: Error during shutdown: %o`, error);
             process.exit(1);
         }
+    };
+
+    process.on('SIGINT', () => gracefulShutdown('SIGINT'));
+    process.on('SIGTERM', () => gracefulShutdown('SIGTERM'));
+    process.on('SIGQUIT', () => gracefulShutdown('SIGQUIT'));
+
+    process.on('warning', (warning: Error) => {
+        logger.warn(`Process warning: %s - %s`, warning.name, warning.message);
     });
 
-    setTimeout(() => {
-        if (!shutdownComplete) {
-            logger.error(
-                '[gracefulShutdown]: Could not close connections in time, forcefully shutting down',
-            );
-            process.exit(1);
-        }
-    }, 10000);
-}
-
-process.on('SIGINT', () => gracefulShutdown('SIGINT'));
-process.on('SIGTERM', () => gracefulShutdown('SIGTERM'));
-process.on('SIGQUIT', () => gracefulShutdown('SIGQUIT'));
-
-process.on('warning', (warning: Error) => {
-    logger.warn(`Process warning: %s - %s`, warning.name, warning.message);
-});
-
-process.on('uncaughtException', async (error: Error, origin: string) => {
-    logger.error(`Uncaught Exception: %o, Origin: %s`, error, origin);
-
-    if (config.app.env === 'production') {
-        try {
-            setTimeout(
-                () =>
-                    sendNotification({
-                        req: {} as Request,
-                        error,
-                    }),
-                0,
-            );
-        } catch (error) {
-            logger.error(`Failed to send uncaught exception notification: %o`, error);
-        }
-    }
-
-    setTimeout(() => gracefulShutdown('UNCAUGHT_EXCEPTION'), 500);
-});
-
-process.on('unhandledRejection', async (reason: unknown, promise: Promise<unknown>) => {
-    if (reason instanceof Error) {
-        logger.error('Unhandled Rejection: %o, Promise: %o', reason, promise);
+    process.on('uncaughtException', async (error: Error, origin: string) => {
+        logger.error(`Uncaught Exception: %o, Origin: %s`, error, origin);
 
         if (config.app.env === 'production') {
             try {
@@ -135,15 +45,43 @@ process.on('unhandledRejection', async (reason: unknown, promise: Promise<unknow
                     () =>
                         sendNotification({
                             req: {} as Request,
-                            error: reason,
+                            error,
                         }),
                     0,
                 );
             } catch (error) {
-                logger.error(`Failed to send unhandled rejection notification: %o`, error);
+                logger.error(`Failed to send uncaught exception notification: %o`, error);
             }
         }
-    } else {
-        logger.error(`Unhandled Rejection: %o, Reason: %o`, promise, reason);
-    }
+
+        setTimeout(() => gracefulShutdown('UNCAUGHT_EXCEPTION'), 500);
+    });
+
+    process.on('unhandledRejection', async (reason: unknown, promise: Promise<unknown>) => {
+        if (reason instanceof Error) {
+            logger.error('Unhandled Rejection: %o, Promise: %o', reason, promise);
+
+            if (config.app.env === 'production') {
+                try {
+                    setTimeout(
+                        () =>
+                            sendNotification({
+                                req: {} as Request,
+                                error: reason,
+                            }),
+                        0,
+                    );
+                } catch (error) {
+                    logger.error(`Failed to send unhandled rejection notification: %o`, error);
+                }
+            }
+        } else {
+            logger.error(`Unhandled Rejection: %o, Reason: %o`, promise, reason);
+        }
+    });
+}
+
+run().catch((error: Error) => {
+    logger.error('Failed to start server: %o', error);
+    process.exit(1);
 });
