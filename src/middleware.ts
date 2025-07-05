a9b3d474681db457e328958a857838ad56e4d492
wajeht <58354193+wajeht@users.noreply.github.com>
2025-07-05 03:44:27 -0500

    style: update card border color in css and html files

--- src/middleware.ts
+++ src/middleware.ts
@@ -196,7 +196,7 @@ export function setupAppLocals(req: Request, res: Response) {
             warning: req.flash ? req.flash('warning') : [],
         },
         version: {
-            style: isProd ? '0.19' : randomNumber,
+            style: isProd ? '0.20' : randomNumber,
             script: isProd ? '0.17' : randomNumber,
         },
     };
