148cf4678032cba012e398751c57a93c6a686d0a
wajeht <58354193+wajeht@users.noreply.github.com>
2025-07-05 04:51:50 -0500

    fix(middleware): update version style to '0.23' for production mode

--- src/middleware.ts
+++ src/middleware.ts
@@ -196,7 +196,7 @@ export function setupAppLocals(req: Request, res: Response) {
             warning: req.flash ? req.flash('warning') : [],
         },
         version: {
-            style: isProd ? '0.22' : randomNumber,
+            style: isProd ? '0.23' : randomNumber,
             script: isProd ? '0.17' : randomNumber,
         },
     };
