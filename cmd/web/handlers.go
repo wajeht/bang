77ebfd73365b9df96bae2920723e3466182a3578
wajeht <58354193+wajeht@users.noreply.github.com>
2025-07-15 00:30:44 -0500

    refactor(cmd/web): remove duplicated error handlers and move to a separate file

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -33,3 +33,13 @@ func (app *application) getFaviconDotIcoHadnler(w http.ResponseWriter, r *http.R
 	w.Header().Set("Content-Type", "image/x-icon")
 	io.Copy(w, f)
 }
+
+func (app *application) notFoundErrorHandler(w http.ResponseWriter, r *http.Request) {
+	message := "The requested resource could not be found"
+	http.Error(w, message, http.StatusNotFound)
+}
+
+func (app *application) serverErrorHandler(w http.ResponseWriter, r *http.Request, err error) {
+	message := "The server encountered a problem and could not process your request"
+	http.Error(w, message, http.StatusInternalServerError)
+}
