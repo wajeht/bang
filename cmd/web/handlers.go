faa1985b3611fbedcd8fdfa3a3dc1ed3aada8a56
wajeht <58354193+wajeht@users.noreply.github.com>
2025-07-15 00:21:23 -0500

    style: update handler functions to use responsewriter for output

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,18 +1,17 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
 func (app *application) getHomePageHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("getHomeHandler()")
+	w.Write([]byte("geHomePageHandler()"))
 }
 
 func (app *application) getRobotsDotTxtHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("getRobotsDotTXTHandler()")
+	w.Write([]byte("getRobotsDotTXTHandler()"))
 }
 
 func (app *application) getFaviconDotIcoHadnler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("getFaviconDotIcoHadnler()")
+	w.Write([]byte("getFaviconDotIcoHadnler()"))
 }
