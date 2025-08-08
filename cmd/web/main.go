f333d046ea719dd95fc1c81e1e233e2249630592
wajeht <58354193+wajeht@users.noreply.github.com>
2025-08-08 01:30:55 -0500

    feat(main): add environment variable retrieval functions for configuration

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,8 +2,41 @@ package main
 
 import (
 	"fmt"
+	"log/slog"
+	"sync"
 )
+
+type app struct {
+	baseUrl  string
+	httpPort int
+}
+
+type email struct {
+	host     string
+	port     int
+	secure   bool
+	user     string
+	password string
+	from     string
+}
+
+type config struct {
+	app   app
+	email email
+}
+
+type application struct {
+	config config
+	logger *slog.Logger
+	wg     sync.WaitGroup
+}
 
 func main() {
 	fmt.Println("bang")
+}
+
+func run(logger  *slog.Logger) error {
+	var cfg config
+
+	cfg.app.baseUrl = env.GetString("BASE_URL", "http://localhsot")
 }
---
+++ internal/env/env.go
@@ -0,0 +1,28 @@
+package env
+
+import (
+	"os"
+	"strconv"
+)
+
+func GetString(key string, defaultValue string) string {
+	value, exists := os.LookupEnv(key)
+	if !exists {
+		return defaultValue
+	}
+	return value
+}
+
+func GetInt(key string, defaultValue int) int {
+	value, exists := os.LookupEnv(key)
+	if !exists {
+		return defaultValue
+	}
+
+	intValue, err := strconv.Atoi(value)
+	if err != nil {
+		panic(err)
+	}
+
+	return intValue
+}
