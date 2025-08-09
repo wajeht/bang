0eaffe084fde6e6d3e9a04e4aadf51b037fad0d3
wajeht <58354193+wajeht@users.noreply.github.com>
2025-08-08 22:53:37 -0500

    feat(errors): add servererror function for handling internal server errors

--- cmd/web/errors.go
+++ cmd/web/errors.go
@@ -9,6 +9,11 @@ func (app *application) notFound(w http.ResponseWriter, r *http.Request) {
 	http.Error(w, message, http.StatusNotFound)
 }
 
+func (app *application) serverError(w http.ResponseWriter, r *http.Request, err error) {
+	message := "The server encountered a problem and could not process your request"
+	http.Error(w, message, http.StatusInternalServerError)
+}
+
 func (app *application) badRequest(w http.ResponseWriter, r *http.Request, err error) {
 	http.Error(w, err.Error(), http.StatusBadRequest)
 }
