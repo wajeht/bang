41c5a45f9fa9c662ee817add1f07d355c5b2410f
wajeht <58354193+wajeht@users.noreply.github.com>
2025-08-10 11:38:41 -0500

    style: update button styles in style.css for better user interaction

--- src/middleware.ts
+++ src/middleware.ts
@@ -214,7 +214,7 @@ export function setupAppLocals(req: Request, res: Response) {
             warning: req.flash ? req.flash('warning') : [],
         },
         version: {
-            style: isProd ? '0.30' : randomNumber,
+            style: isProd ? '0.31' : randomNumber,
             script: isProd ? '0.19' : randomNumber,
         },
     };
