83ffe9eced92a5c6854c3d410941340a811e611f
wajeht <58354193+wajeht@users.noreply.github.com>
2025-08-10 12:42:14 -0500

    style(src): update version style in middleware to '0.32' for production

--- src/middleware.ts
+++ src/middleware.ts
@@ -214,7 +214,7 @@ export function setupAppLocals(req: Request, res: Response) {
             warning: req.flash ? req.flash('warning') : [],
         },
         version: {
-            style: isProd ? '0.31' : randomNumber,
+            style: isProd ? '0.32' : randomNumber,
             script: isProd ? '0.19' : randomNumber,
         },
     };
