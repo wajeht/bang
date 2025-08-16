a9108d29c58d778317c94c85092a134a7d8cfe02
wajeht <58354193+wajeht@users.noreply.github.com>
2025-08-16 03:46:09 -0500

    refactor(app): refactor app.ts for dynamic import of expresstemplatesreload

--- src/app.ts
+++ src/app.ts
@@ -139,11 +139,9 @@ export async function createServer() {
             case 'EACCES':
                 logger.error(`${bind} requires elevated privileges`);
                 process.exit(1);
-                break;
             case 'EADDRINUSE':
                 logger.error(`${bind} is already in use`);
                 process.exit(1);
-                break;
             default:
                 throw error;
         }
