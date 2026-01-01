3c53eae02dbf93053c793791648a6537eb9d0459
wajeht <58354193+wajeht@users.noreply.github.com>
2025-12-31 21:55:07 -0600

    ci: improve ci by skipping db prepare and using dev:only script

--- playwright.config.ts
+++ playwright.config.ts
@@ -64,7 +64,7 @@ export default defineConfig({
 
     /* Run your local dev server before starting the tests */
     webServer: {
-        command: 'npm run dev',
+        command: process.env.CI ? 'npm run dev:only' : 'npm run dev',
         url: `http://127.0.0.1:${config.app.port}`,
         reuseExistingServer: !process.env.CI,
         timeout: process.env.CI ? 120_000 : 60_000,
